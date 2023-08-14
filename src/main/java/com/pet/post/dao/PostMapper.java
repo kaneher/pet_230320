0835e4b8b93ec005771aa50b3256252ae2b3a832
test <test@test.com>
2023-08-14 16:24:00 +0900

    pet_230320 #5 Post 마크업 / API

---
+++ src/main/java/com/pet/post/dao/PostMapper.java
@@ -0,0 +1,46 @@
+package com.pet.post.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.pet.post.domain.Post;
+
+// MyBatis
+@Repository
+public interface PostMapper {
+	public List<Map<String, Object>> selectPostList();
+	
+	public List<Post> selectPostListByUserId(
+			@Param("userId") int userId, 
+			@Param("direction") String direction,
+			@Param("standardId") Integer standardId,
+			@Param("limit") int limit);
+	
+	public int selectPostIdByUserIdAndSort(
+			@Param("userId") int userId,
+			@Param("sort") String sort);
+	
+	public int insertPost(
+			@Param("userId") int userId, 
+			@Param("subject") String subject, 
+			@Param("content") String content,
+			@Param("imagePath") String imagePath);
+	
+	public Post selectPostByPostIdAndUserId(
+			@Param("postId") int postId111111111, 
+			@Param("userId") int userId);
+	
+	public void updatePostByPostIdAndUserId(
+			@Param("postId") int postId, 
+			@Param("userId") int userId, 
+			@Param("subject") String subject, 
+			@Param("content") String content,
+			@Param("imagePath") String imagePath);
+	
+	public void deletePostByPostIdAndUserId(
+			@Param("postId") int postId, 
+			@Param("userId") int userId);
+}
